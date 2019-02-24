e9fada6c17e25a99665b6d740968126d89f397af
ComradeIvan <ivan.shadow25@gmail.com>
2019-02-24 18:44:30 -0500

    Create IsLessThan.php

---
+++ package/Rules/IsLessThan.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Arbiter\Rules;
+
+use Arbiter\Contracts\Context;
+use Arbiter\Contracts\CustomValueRule;
+use Arbiter\Core\Rule;
+
+abstract class IsLessThan extends Rule implements CustomValueRule
+{
+    protected $ceiling;
+
+    public function __construct($ceiling)
+    {
+        $this->ceiling = $ceiling;
+    }
+
+    /**
+     * Evaluates the rule
+     *
+     * @param Context $context
+     * @return bool
+     */
+    public function evaluate(Context $context)
+    {
+        return $this->getValue($context) < $this->ceiling;
+    }
+
+    /**
+     * Returns an ordered array of parameters
+     *
+     * @return mixed
+     */
+    public function getNormalizedParameters()
+    {
+        return [
+            'ceiling' => $this->ceiling,
+        ];
+    }
+}
